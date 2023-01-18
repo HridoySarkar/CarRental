85bd3fe8c169fa8fc71c526c387df881d7e34447
Hridoy <khanh4572@gmail.com>
2023-01-18 22:47:06 +0600

    View files

---
+++ view/aboutUs.php
@@ -0,0 +1,28 @@
+<?php
+if(isset($_SESSION['admin'])){
+include 'config/adminPageConfig.php';
+include 'partials/adminNav.php';
+}elseif(isset($_SESSION['user'])){
+include 'config/userPageConfig.php';
+include 'partials/userNav.php';
+}else{
+    include 'partials/_dbconnect.php';
+    include 'partials/navbar.php';
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+include 'partials/footer.php';
+?>
